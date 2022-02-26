26ad111bc8a27073324ac4dea371ad7438ec5ffb
Thien-Bao Bui <buithienbao50@gmail.com>
2022-02-26 00:06:46 -0500

    Added a binSearch function, plus test cases.

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -39,6 +39,29 @@ class transactionTree:
 
 class transactionMapping:
 
+    ## Binary search implementation
+    ## NOTE: Left and right take in index values. 
+    def binSearch(self, arr, target): 
+        left = 0 
+        right = len(arr) - 1
+
+        while left <= right: 
+            mid = left + (right - left) // 2
+
+            # Base case: Checks if target value is found in the middle. 
+            if arr[mid] == target: 
+                return mid # Returns an index value
+            
+            # Disregard left half of array if target is greater... 
+            elif arr[mid] < target: 
+                left = mid + 1
+
+            # Disregard right half of array if target is less than... 
+            elif arr[mid] > target: 
+                right = mid - 1
+
+        return None
+
     def createTransactionTree(self,dataset,length):
 
 
@@ -62,7 +85,7 @@ class transactionMapping:
                     freqent_itemset[data[j]] = 1
         
         ##
-        ## Sorting based on the values and delete the ones with frequency as  1
+        ## Sorting frequent itemset and deleting values with a frequency of 1. 
         ##
     
         freqent_itemset_sorted = sorted(freqent_itemset.items(), key = lambda kv:(kv[1], kv[0]))
@@ -85,10 +108,27 @@ class transactionMapping:
             ## Since the 'frequent_itemset_sorted' is sorted we can query the elements from data into
             ## 'frequent_itemset_sorted' using Binary Search
             ##
-            
-        
+             
+            # Worse case O(n) 
             # for keys in freqent_itemset_sorted:
-            #     if keys in data:
+            #     if keys in data: 
             #         ordered_frequent_itemset[dataset[i].t_id].append(keys)
-                    
-                
+            
+            # Since "frequent_itemset_sorted" is sorted... We can query elements
+            # from 'data' into "ordered_frequent_itemset" using binSearch.
+            # Worse case O(log(n))
+            for keys in freqent_itemset_sorted:
+                key = binSearch(data, keys):
+                    ordered_frequent_itemset[dataset[i].t_id].append(keys)
+
+
+if __name__ == '__main__': 
+    # Testing binSearch method
+    L = [1,2,3,4,5,6,7,8,9,10] 
+    tar1 = 6
+    tar2 = -3
+    bS = transactionMapping()
+    assert bS.binSearch(L, tar1) == 5
+    assert bS.binSearch(L, tar2) == None
+    print("binSearch test cases passed!")
+
