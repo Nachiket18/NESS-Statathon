8015e3eb6c5165d852123eca279bbdbd34af3e25
Rany Kamel <rany.kamel47@gmail.com>
2022-07-22 18:38:21 -0400

    H_m is returned by tree traversal
    nodes_at_level(col) returns H_m, where m = col, using recursive tree traversal.

--- src/lexicographic_tree.py
+++ src/lexicographic_tree.py
@@ -20,6 +20,16 @@ class LexicoNode:
                 print("\n", _indent(depth + 1), "---", i)
                 self.children[i].print_out(depth + 1)
 
+    def _trav(self, targ_col: int, level: int = 0):
+        if level >= targ_col:
+            return self.data
+        elif level < targ_col:
+            items = [x._trav(targ_col, level + 1) for x in self.children.values()]
+            return [i for flat_l in items for i in flat_l] #flattens list
+
+    def nodes_at_level(self, col: int):
+        return self._trav(col - 1)
+
 
 if __name__ == "__main__":
     lex = LexicoNode([2, 3, 5, 20], {0: LexicoNode([69], {})})
@@ -27,10 +37,12 @@ if __name__ == "__main__":
     # lex2 = LexicoNode()
 
     lex.add_node(4, LexicoNode([10, 23, 34], {1: LexicoNode([50], {})}))
-    lex.add_node(6, LexicoNode([10, 23, 34], {1: LexicoNode([50], {})}))
+    lex.add_node(6, LexicoNode([10, 21, 34], {1: LexicoNode([50], {})}))
     print(lex.__dict__)
 
     print(lex.print_out())
+
+    print(lex.nodes_at_level(1))
 
     # lex = LexicographicTree()
 
